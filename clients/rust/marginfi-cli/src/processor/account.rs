29862d58f983b5da8c63641af178a6ac498af8a6
Femi Bolaji <bolajifemi28@gmail.com>
2026-03-15 13:12:13 +0100

    cargo fmt

--- clients/rust/marginfi-cli/src/processor/account.rs
+++ clients/rust/marginfi-cli/src/processor/account.rs
@@ -673,11 +673,7 @@ pub fn marginfi_account_place_order(
             system_program: system_program::id(),
         }
         .to_account_metas(Some(true)),
-        data: marginfi::instruction::MarginfiAccountPlaceOrder {
-            bank_keys,
-            trigger,
-        }
-        .data(),
+        data: marginfi::instruction::MarginfiAccountPlaceOrder { bank_keys, trigger }.data(),
     };
 
     let signing_keypairs = config.get_signers(false);
