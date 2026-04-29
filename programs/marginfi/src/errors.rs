390dae2366e0d6e05797cfef34db6f21dda8754a
Jon Gurary <91919816+jgur-psyops@users.noreply.github.com>
2026-04-29 16:27:29 -0400

    Add close_liquidation_record, update close account (#564)
    * Add close_liquidation_record instruction 
    
    * Adds a counter for active orders
    
    * Now requires Records and Orders to be closed before accounts to make it less likely for them to be orphaned.
    
    Co-authored-by: Russel <alexandre@russel.fr>

--- programs/marginfi/src/errors.rs
+++ programs/marginfi/src/errors.rs
@@ -218,8 +218,7 @@ pub enum MarginfiError {
     LiquidatorOrderCloseNotAllowed,
     #[msg("Order trigger is yet to be met")] // 6107
     OrderTriggerNotMet,
-    #[msg("Order execution state issue. Check the necessary invariants i.e not in flashloan or disabled e.t.c")]
-    // 6108
+    #[msg("Order execution state issue. Check not in flashloan, disabled, etc")] // 6108
     UnexpectedOrderExecutionState,
     #[msg("Order liability not closed")] // 6109
     OrderLiabilityNotClosed,
