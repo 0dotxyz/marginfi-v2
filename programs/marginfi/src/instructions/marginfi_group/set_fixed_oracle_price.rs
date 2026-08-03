3d420ac391944aa85712a28d09312cff05cb75c9
ashleychandy <ashleychandy777@gmail.com>
2026-08-03 20:38:04 +0000

    replace hardcoded sysvar address with sysvar crate

--- programs/marginfi/src/instructions/marginfi_group/set_fixed_oracle_price.rs
+++ programs/marginfi/src/instructions/marginfi_group/set_fixed_oracle_price.rs
@@ -95,6 +95,7 @@ pub struct LendingPoolSetFixedOraclePrice<'info> {
     )]
     pub bank: AccountLoader<'info, Bank>,
 
-    #[account(address = pubkey!("Sysvar1nstructions4gQvDKZeTQvzK88j5KqVn5P"))]
+    /// CHECK: instruction sysvar
+    #[account(address = solana_instructions_sysvar::id())]
     pub instruction_sysvar: UncheckedAccount<'info>,
 }
